2ff012b1b2df5dcc980b958fa8d1937a5c81f9ed
ItsSilva <juanse909@hotmail.com>
2025-10-15 16:56:59 -0500

    Movies page add

--- my-react-app/src/pages/Movies.tsx
+++ my-react-app/src/pages/Movies.tsx
@@ -1,5 +1,57 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { useGetMoviesQuery } from "../services/ghibliFetch";
+import type { RootState } from "../redux/store";
+import { deleteMovie, getMovies } from "../redux/slices/GetMoviesSlice";
+import { useEffect } from "react";
+
 const Movies = () => {
-  return <></>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { data, isLoading, error } = useGetMoviesQuery(10);
+  console.log(data);
+  const movies = useSelector((state: RootState) => state.movies.movies);
+
+  useEffect(() => {
+    if (data) {
+      dispatch(getMovies(data));
+    }
+  }, [data, dispatch]);
+
+  const handleDelete = (id: string) => {
+    dispatch(deleteMovie(id));
+  };
+
+  const handleUpdate = (id: string) => {
+    navigate(`/update-movie/${id}`);
+  };
+
+  return (
+    <section>
+      <h1>Add Movie</h1>
+      <button onClick={() => navigate("/add")}>Add Movie</button>
+
+      {isLoading && movies.length === 0 ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error fetching movies</p>
+      ) : movies.length > 0 ? (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <p>{movie.director}</p>
+              <p>{movie.year}</p>
+              <button onClick={() => handleDelete(movie.id)}>Delete</button>
+              <button onClick={() => handleUpdate(movie.id)}>Update</button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No movies available</p>
+      )}
+    </section>
+  );
 };
 
 export default Movies;
