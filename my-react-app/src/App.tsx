7f9532765b65a1004ab919350e27b2fdd152cbec
ItsSilva <juanse909@hotmail.com>
2025-10-15 16:57:05 -0500

    App update

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Movies from "./pages/Movies";
+import EditMovie from "./pages/EditMovie";
+import AddMovie from "./pages/AddMovie";
+
 const App = () => {
-
   return (
-    <>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/edit-movie/:id" element={<EditMovie />} />
+        <Route path="/add" element={<AddMovie />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
