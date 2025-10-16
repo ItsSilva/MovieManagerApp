64d4a131d5283074b59b5c40444738374f58da91
ItsSilva <juanse909@hotmail.com>
2025-10-16 00:10:37 -0500

    edit page created

--- my-react-app/src/pages/EditMovie.tsx
+++ my-react-app/src/pages/EditMovie.tsx
@@ -1,8 +1,60 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import { updateMovie } from "../redux/slices/GetMoviesSlice";
+
 const EditMovie = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [director, setDirector] = useState("");
+  const [year, setYear] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (title.trim() && director.trim() && year.trim() && id) {
+      const editMovie = {
+        id: id,
+        title: title.trim(),
+        director: director.trim(),
+        year: year.trim(),
+      }
+      dispatch(updateMovie(editMovie));
+      navigate("/");
+    }
+  }
+
   return (
     <div>
       <h1>Edit Movie</h1>
-      {/* Form for editing movie details */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title:</label>
+          <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          />
+          <label>Director:</label>
+          <input
+          type="text"
+          value={director}
+          onChange={(e) => setDirector(e.target.value)}
+          required
+          />
+          <label>Year:</label>
+          <input 
+          type="text" 
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+          required
+          />
+        </div>
+        <button type="submit">Add Movie</button>
+      </form>
     </div>
   );
 };
